c6daf8777fd2d573d17ac63c87f69c8af8594235
s25692 <s25692@pjwstk.edu.pl>
2024-03-10 18:44:20 +0100

    added count average

--- Zadanie_1/Program.cs
+++ Zadanie_1/Program.cs
@@ -6,4 +6,9 @@
 
         List<int> valuesList = new List<Int32>() { 1, 2, 3, 4, 5 };
     }
+    
+    public static double CountAverage(List<int> values)
+    {
+        return values.Average();
+    }
 }
