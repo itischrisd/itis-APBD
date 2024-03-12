a14dc5a508b14193de3b334bb0a2441249eae957
s25692 <s25692@pjwstk.edu.pl>
2024-03-12 23:29:23 +0100

    changed variable name to sum1

--- Zadanie_1/Program.cs
+++ Zadanie_1/Program.cs
@@ -1,12 +1,12 @@
 ﻿namespace Zadanie_1
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
 
-            List<int> valuesList = new List<Int32>() { 1, 2, 3, 4, 5 };
+            List<int> valuesList = [1, 2, 3, 4, 5];
 
             Console.WriteLine($"Average is: {CountAverage(valuesList)}");
             Console.WriteLine($"Max value is: {CountMax(valuesList)}");
@@ -14,13 +14,13 @@
 
         private static double CountAverage(List<int> values)
         {
-            var sum = 0;
+            var sum1 = 0;
             foreach (var value in values)
             {
-                sum += value;
+                sum1 += value;
             }
 
-            return (double)sum / values.Count;
+            return (double)sum1 / values.Count;
         }
 
         private static int CountMax(List<int> values)
