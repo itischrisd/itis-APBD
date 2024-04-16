1406c2842ad9f007c247f3fde4801f2c55c2e767
Krzysztof Dudek <dudek.krzysztof94@gmail.com>
2024-04-16 09:29:18 +0200

    implement unit tests for AnimalService

---
+++ LABO03/AnimalsAppTest/AnimalServiceTest.cs
@@ -0,0 +1,141 @@
+﻿using System.Linq;
+using AnimalsAppTest.TestDoubles;
+using JetBrains.Annotations;
+using WebApplication1.DTOs;
+using WebApplication1.Repositories;
+using WebApplication1.Services;
+using Xunit;
+
+namespace AnimalsAppTest;
+
+[TestSubject(typeof(AnimalService))]
+public class AnimalServiceTest()
+{
+
+    [Fact]
+    public void GetAnimals_Should_Return_Animals_Sorted_By_Name()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        animalService.GetAnimals("name");
+
+        Assert.Equal("Elephant", animalService.GetAnimals("name").First().Name);
+        Assert.Equal("Lion", animalService.GetAnimals("name").ElementAt(1).Name);
+        Assert.Equal("Penguin", animalService.GetAnimals("name").Last().Name);
+        Assert.Equal(3, animalService.GetAnimals("name").Count());
+    }
+
+    [Fact]
+    public void GetAnimals_Should_Return_Animals_Sorted_By_Id()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        animalService.GetAnimals("id");
+
+        Assert.Equal("Lion", animalService.GetAnimals("id").First().Name);
+        Assert.Equal("Elephant", animalService.GetAnimals("id").ElementAt(1).Name);
+        Assert.Equal("Penguin", animalService.GetAnimals("id").Last().Name);
+        Assert.Equal(3, animalService.GetAnimals("id").Count());
+    }
+
+    [Fact]
+    public void GetAnimal_Should_Return_Animal_With_Correct_Id()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var animal = animalService.GetAnimal(2);
+
+        Assert.Equal(2, animal.Id);
+        Assert.Equal("Elephant", animal.Name);
+        Assert.Equal("Large land animal", animal.Description);
+        Assert.Equal("Mammal", animal.Category);
+        Assert.Equal("Asia", animal.Area);
+    }
+
+    [Fact]
+    public void GetAnimal_Should_Return_Null_When_Animal_Not_Found()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var animal = animalService.GetAnimal(4);
+
+        Assert.Null(animal);
+    }
+
+    [Fact]
+    public void CreateAnimal_Should_Return_Created_Animal_Id()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var id = animalService.CreateAnimal(new AnimalCreationDto
+        {
+            Name = "Tiger",
+            Description = "Large cat",
+            Category = "Mammal",
+            Area = "Asia"
+        });
+
+        Assert.Equal(4, id);
+    }
+
+    [Fact]
+    public void UpdateAnimal_Should_Return_1_When_Animal_Updated()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var result = animalService.UpdateAnimal(2, new AnimalUpdateDto
+        {
+            Name = "Elephant",
+            Description = "Large land animal",
+            Category = "Mammal",
+            Area = "Asia"
+        });
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void UpdateAnimal_Should_Return_0_When_Animal_Not_Found()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var result = animalService.UpdateAnimal(4, new AnimalUpdateDto
+        {
+            Name = "Tiger",
+            Description = "Large cat",
+            Category = "Mammal",
+            Area = "Asia"
+        });
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void DeleteAnimal_Should_Return_1_When_Animal_Deleted()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var result = animalService.DeleteAnimal(2);
+
+        Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void DeleteAnimal_Should_Return_0_When_Animal_Not_Found()
+    {
+        var animalRepository = new FakeAnimalReposiotry();
+        var animalService = new AnimalService(animalRepository);
+
+        var result = animalService.DeleteAnimal(4);
+
+        Assert.Equal(0, result);
+    }
+}
---
+++ LABO03/AnimalsAppTest/TestDoubles/FakeAnimalReposiotry.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApplication1.Models;
+using WebApplication1.Repositories;
+
+namespace AnimalsAppTest.TestDoubles;
+
+public class FakeAnimalReposiotry : IAnimalRepository
+{
+    private IEnumerable<Animal> _animals = new List<Animal>
+    {
+        new() { Id = 1, Name = "Lion", Description = "The king of the jungle", Category = "Mammal", Area = "Africa" },
+        new() { Id = 2, Name = "Elephant", Description = "Large land animal", Category = "Mammal", Area = "Asia" },
+        new() { Id = 3, Name = "Penguin", Description = "A flightless bird", Category = "Bird", Area = "Antarctica" }
+    };
+
+    public IEnumerable<Animal> GetAnimals(string orderBy)
+    {
+        return orderBy.ToLower() switch
+        {
+            "id" => _animals.OrderBy(a => a.Id),
+            "category" => _animals.OrderBy(a => a.Category),
+            "description" => _animals.OrderBy(a => a.Description),
+            "area" => _animals.OrderBy(a => a.Area),
+            _ => _animals.OrderBy(a => a.Name)
+        };
+    }
+
+    public Animal GetAnimal(int id)
+    {
+        return _animals.FirstOrDefault(a => a.Id == id)!;
+    }
+
+    public int CreateAnimal(Animal animal)
+    {
+        _animals = _animals.Append(new Animal()
+        {
+            Id = _animals.Max(a => a.Id) + 1,
+            Name = animal.Name,
+            Description = animal.Description,
+            Category = animal.Category,
+            Area = animal.Area
+        });
+        return _animals.Max(a => a.Id);
+    }
+
+    public int UpdateAnimal(Animal animal)
+    {
+        var animalToUpdate = _animals.FirstOrDefault(a => a.Id == animal.Id);
+        if (animalToUpdate == null)
+        {
+            return 0;
+        }
+
+        _animals = _animals.Select(a => a.Id == animal.Id ? animal : a);
+        return 1;
+    }
+
+    public int DeleteAnimal(int id)
+    {
+        var animalToDelete = _animals.FirstOrDefault(a => a.Id == id);
+        if (animalToDelete == null)
+        {
+            return 0;
+        }
+
+        _animals = _animals.Where(a => a.Id != id);
+        return 1;
+    }
+}
