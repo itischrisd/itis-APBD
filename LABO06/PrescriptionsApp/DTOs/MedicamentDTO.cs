692d4589651715de174e55bc9b9f613be247981e
Krzysztof Dudek <s25692@pjwstk.edu.pl>
2024-05-29 12:38:34 +0200

    moved exercise 6

--- LABO06/PrescriptionsApp/DTOs/MedicamentDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication1.DTOs;
-
-public class MedicamentDTO
-{
-    public string ID { get; set; } = null!;
-    public string Dose { get; set; } = null!;
-    public string Description { get; set; } = null!;
-}
